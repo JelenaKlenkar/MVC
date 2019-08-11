d31cdd0d29e9b9a65e7826c3ad745502e757692b
JelenaKlenkar <jelena.klenkar@gmail.com>
2019-08-11 23:07:25 +0200

    added menu in Start Class

--- SummerProject_NetBeans/src/main/java/klenkar/summertask/Start.java
+++ SummerProject_NetBeans/src/main/java/klenkar/summertask/Start.java
@@ -7,13 +7,18 @@ package klenkar.summertask;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Jelena
  */
 public class Start {
     private List<Applicant> applicants= new ArrayList<Applicant>();
+    private Connection connection;
     public Start(){
         applicants=new ArrayList<>();
         main_loop:
@@ -22,6 +27,14 @@ public class Start {
         switch(Helper.enterInteger("Enter number from menu: ")){
             case 1:
                 listApplicants();
+                break;
+            case 2:
+                applicants.add(enterApplicant());
+                break;
+            case 3:
+                editApplicant();
+                break;
+                
         }
     }
         
@@ -55,8 +68,91 @@ public class Start {
 
     private void listApplicants() {
        connection=ConnectToDatabase.getConnection();
-       
-           
+       try{
+           PreparedStatement expression=connection.prepareStatement("Selest * from applicants");
+           ResultSet rs=expression.executeQuery();
+           while(rs.next()){
+               System.out.println(rs.getInt("id") + " " + rs.getString("firstName") + " " + rs.getString("lastName"));
+           }
+           rs.close();
+
+       }catch(SQLException e){
+           System.out.println(e.getMessage());
        }
+    }
+
+    private Applicant enterApplicant() {
+       connection=ConnectToDatabase.getConnection();
+       Applicant a=new Applicant();
+       a=setValues(a);
+       String sql="INSERT INTO APPLICANT" 
+               +"(id,firstName,lastName,address,phoneNumber,email,personalIdentificationNumber,applicantCV,motivationalLetter)"
+               +"VALUES(?,?,?,?,?,?,?,?,?)";
+       try{
+           PreparedStatement expression=connection.prepareStatement(sql);
+           expression.setNull(1, java.sql.Types.INTEGER);
+           expression.setString(2,a.getFirstName());
+           expression.setString(3,a.getLastName());
+           expression.setString(4, a.getAddress());
+           expression.setInt(5, a.getPhoneNumber());
+           expression.setString(6,a.getEmail());
+           expression.setInt(7,a.getPersonalIdentificationNumber());
+           expression.setString(8, a.getApplicantCV());
+           expression.setString(10, a.getMotivationalLetter());
+       }catch(Exception e){
+           e.printStackTrace();
+       }
+       return a;
+    }
+
+    private void editApplicant() {
+        connection= ConnectToDatabase.getConnection();
+        listApplicants();
+        int id=ordinalNumberOfApplicant();
+        Applicant a =new Applicant();
+    try{
+        PreparedStatement expression=connection.prepareStatement("Select * from applicant where id= '" +id +";'");
+        ResultSet rs=expression.executeQuery();
+        while(rs.next()){
+            System.out.println("First name: " +rs.getString("firstName"));
+            System.out.println("Last name: " + rs.getString("lastName"));
+            System.out.println("Address: " +rs.getString("address"));
+            System.out.println("Phone number: " + rs.getInt("phoneNumber"));
+            System.out.println("Email: " +rs.getString("email"));
+            System.out.println("Personal identification number: " + rs.getInt("personalIdentificationNumber"));
+            System.out.println("Applicant CV: " + rs.getString("applicantCV"));
+            System.out.println("Motivational letter: " +rs.getString("motivationalLetter"));
+        }
+        }catch(SQLException e){
+               e.printStackTrace();
+                }
+        a=setValues(a);
+   try{
+       PreparedStatement expressionUpdate=connection.prepareStatement("update applicant set firstName=?,"
+               + "lastName=?,address=?,phonenumber=?,email=?,personalIdentificationNumber=?,applicantCV=?,motivationalLetter=?"
+               + "where id='"+id+"';");
+       expressionUpdate.setString(1,a.getFirstName());
+       expressionUpdate.setString(2,a.getLastName());
+       expressionUpdate.setString(3, a.getAddress());
+       expressionUpdate.setInt(4,a.getPhoneNumber());
+       expressionUpdate.setString(5,a.getEmail());
+       expressionUpdate.setInt(6, a.getPersonalIdentificationNumber());
+       expressionUpdate.setString(7,a.getApplicantCV());
+       expressionUpdate.setString(8, a.getMotivationalLetter());
+   }catch(SQLException e){
+       System.out.println(e.getMessage());
+       }
+    }
+
+    private int ordinalNumberOfApplicant() {
+        int on;
+        while(true){
+            on=Helper.enterInteger("Enter ordinal number");
+            if(on<0){
+                JOptionPane.showConfirmDialog(null, "Entering ordinal number required");
+                continue;
+            }
+            return on;
+        }
     }
 }
