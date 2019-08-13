73432a7e98cc1dc72f3293a13e8182a91d9c5af5
JelenaKlenkar <jelena.klenkar@gmail.com>
2019-08-13 16:49:28 +0200

    formatted code

--- SummerProject_NetBeans/src/main/java/klenkar/summertask/ConnectToDatabase.java
+++ SummerProject_NetBeans/src/main/java/klenkar/summertask/ConnectToDatabase.java
@@ -13,11 +13,12 @@ import java.sql.DriverManager;
  * @author Jelena
  */
 public class ConnectToDatabase {
-    public static Connection getConnection(){
-        try{
+
+    public static Connection getConnection() {
+        try {
             Class.forName("org.mariadb.jdbc.Driver");
-            return DriverManager.getConnection("jdbc:mariadb://localhost/human_resources","jelena","jelena");
-        }catch(Exception e){
+            return DriverManager.getConnection("jdbc:mariadb://localhost/human_resources", "jelena", "jelena");
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
