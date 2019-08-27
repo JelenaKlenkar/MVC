43d561bbabaea7f8f10751f653568ed3ee43a7fe
JelenaKlenkar <jelena.klenkar@gmail.com>
2019-08-27 23:02:15 +0200

    added new project MVC

---
+++ MVC/src/main/java/human_resources/Start.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package human_resources;
+
+import human_resources.controller.ProcessingApplicant;
+import human_resources.model.Applicant;
+import human_resources.utility.JelenaException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jelena
+ */
+public class Start {
+
+    public static void main(String[] args) {
+
+        Applicant a = new Applicant();
+
+        a.setFirstName(JOptionPane.showInputDialog("Enter first name: "));
+        a.setLastName(JOptionPane.showInputDialog("Enter last name: "));
+        a.setAddress(JOptionPane.showInputDialog("Enter address: "));
+        a.setEmail(JOptionPane.showInputDialog("Enter email: "));
+        
+        ProcessingApplicant pa=new ProcessingApplicant();
+        
+        try {
+            pa.create(a);
+        } catch (JelenaException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
